fa989c058d5710193085ee7ea3fe38b8073bc97b
yeshchyrova <e.shchyrova@gmail.com>
2024-05-18 17:04:26 +0200

    test deploy1.0

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+mongoose.Promise = global.Promise;
+
+const PORT = process.env.PORT || 9000;
+const eliftechDb =
+  "mongodb+srv://elisarrar:VyfZ6X8abwx1883n@cluster0.med7ycc.mongodb.net/db-events?retryWrites=true&w=majority&appName=Cluster0";
+// mongoose.set("strictQuery", true);
 
 // req.params.id
 // req.query === {page: 1, sdsd: 34}
@@ -17,11 +24,8 @@ app.use(express.json());
 app.use(logger(formatsLogger));
 app.use(express.static("public"));
 
-// app.use("/", eventsRouter);
-app.use("/", (req, res) => {
-  res.send("EE server was run (send method)");
-  console.log("EE server was run")
-})
+app.use("/", eventsRouter);
+
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
 });
@@ -30,5 +34,18 @@ app.use((err, _, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+
+mongoose
+  .connect(eliftechDb)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+    console.log("Database connection successful");
+  })
+  .catch((e) => {
+    console.log(e.message);
+    process.exit(1);
+  });
 
 module.exports = { app };
--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
-const { app } = require("./app");
+// const mongoose = require("mongoose");
+// const { app } = require("./app");
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-const PORT = process.env.PORT || 8080;
-const eliftechDb =
-  "mongodb+srv://elisarrar:VyfZ6X8abwx1883n@cluster0.med7ycc.mongodb.net/db-events?retryWrites=true&w=majority&appName=Cluster0";
+// const PORT = process.env.PORT || 8080;
+// const eliftechDb =
+//   "mongodb+srv://elisarrar:VyfZ6X8abwx1883n@cluster0.med7ycc.mongodb.net/db-events?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
 
-mongoose
-  .connect(eliftechDb)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    });
-    console.log("Database connection successful");
-  })
-  .catch((e) => {
-    console.log(e.message);
-    process.exit(1);
-  });
+// mongoose
+//   .connect(eliftechDb)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server running. Use our API on port: ${PORT}`);
+//     });
+//     console.log("Database connection successful");
+//   })
+//   .catch((e) => {
+//     console.log(e.message);
+//     process.exit(1);
+//   });
